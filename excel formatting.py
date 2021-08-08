e45522cde229ad8322e6c49c2bdaf0aba3af95ce
MattK4517 <rotmgmayhem@gmail.com>
2021-08-08 17:05:49 -0400

    general changes

---
+++ excel formatting.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import pymongo
+from main import client
+
+
+godsDict = {
+    "Achilles": 0,
+    "Agni": 0,
+    "Ah Muzen Cab": 0,
+    "Ah Puch": 0,
+    "Amaterasu": 0,
+    "Anhur": 0,
+    "Anubis": 0,
+    "Ao Kuang": 0,
+    "Aphrodite": 0,
+    "Apollo": 0,
+    "Arachne": 0,
+    "Ares": 0,
+    "Artemis": 0,
+    "Artio": 0,
+    "Athena": 0,
+    "Awilix": 0,
+    "Baba Yaga": 0,
+    "Bacchus": 0,
+    "Bakasura": 0,
+    "Baron Samedi": 0,
+    "Bastet": 0,
+    "Bellona": 0,
+    "Cabrakan": 0,
+    "Camazotz": 0,
+    "Cerberus": 0,
+    "Cernunnos": 0,
+    "Chaac": 0,
+    "Chang\'e": 0,
+    "Chernobog": 0,
+    "Chiron": 0,
+    "Chronos": 0,
+    "Cthulhu": 0,
+    "Cu Chulainn": 0,
+    "Cupid": 0,
+    "Da Ji": 0,
+    "Danzaburou": 0,
+    "Discordia": 0,
+    "Erlang Shen": 0,
+    "Eset": 0,
+    "Fafnir": 0,
+    "Fenrir": 0,
+    "Freya": 0,
+    "Ganesha": 0,
+    "Geb": 0,
+    "Gilgamesh": 0,
+    "Guan Yu": 0,
+    "Hachiman": 0,
+    "Hades": 0,
+    "He Bo": 0,
+    "Heimdallr": 0,
+    "Hel": 0,
+    "Hera": 0,
+    "Hercules": 0,
+    "Horus": 0,
+    "Hou Yi": 0,
+    "Hun Batz": 0,
+    "Izanami": 0,
+    "Janus": 0,
+    "Jing Wei": 0,
+    "Jormungandr": 0,
+    "Kali": 0,
+    "Khepri": 0,
+    "King Arthur": 0,
+    "Kukulkan": 0,
+    "Kumbhakarna": 0,
+    "Kuzenbo": 0,
+    "Loki": 0,
+    "Medusa": 0,
+    "Mercury": 0,
+    "Merlin": 0,
+    "Morgan Le Fay": 0,
+    "Mulan": 0,
+    "Ne Zha": 0,
+    "Neith": 0,
+    "Nemesis": 0,
+    "Nike": 0,
+    "Nox": 0,
+    "Nu Wa": 0,
+    "Odin": 0,
+    "Olorun": 0,
+    "Osiris": 0,
+    "Pele": 0,
+    "Persephone": 0,
+    "Poseidon": 0,
+    "Ra": 0,
+    "Raijin": 0,
+    "Rama": 0,
+    "Ratatoskr": 0,
+    "Ravana": 0,
+    "Scylla": 0,
+    "Serqet": 0,
+    "Set": 0,
+    "Skadi": 0,
+    "Sobek": 0,
+    "Sol": 0,
+    "Sun Wukong": 0,
+    "Susano": 0,
+    "Sylvanus": 0,
+    "Terra": 0,
+    "Thanatos": 0,
+    "The Morrigan": 0,
+    "Thor": 0,
+    "Thoth": 0,
+    "Tiamat": 0,
+    "Tsukuyomi": 0,
+    "Tyr": 0,
+    "Ullr": 0,
+    "Vamana": 0,
+    "Vulcan": 0,
+    "Xbalanque": 0,
+    "Xing Tian": 0,
+    "Yemoja": 0,
+    "Ymir": 0,
+    "Zeus": 0,
+    "Zhong Kui": 0
+}
+
+dataSheet = pd.read_excel("God Abilities & Items.xlsx", sheet_name="god_abilties_all")
+
+formattedDS = dict(dataSheet)
+
+
+
+toMongo = {}
+index = 0
+for god in formattedDS["god_name"]:
+    toMongo[god] = {"Abilities": [], "Abilities_urls": []}
+
+for god in formattedDS["god_name"]:
+    if formattedDS["god_name"][index] == god:
+        toMongo[god]["Abilities"].append(formattedDS["ability_name"][index])
+        toMongo[god]["Abilities_urls"].append(formattedDS["ability_url"][index])
+    index += 1
+
+mydb = client["URLS"]
+for god in godsDict.keys(): 
+    if god != "Morgan Le Fay":
+        mycol = mydb[god]
+        mycol.insert_one(toMongo[god])
