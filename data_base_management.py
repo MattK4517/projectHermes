801e7e4a653ae29fd4e405a3192998d39f5dd70f
Matt <rotmgmayhem@gmail.com>
2021-08-17 13:16:20 -0400

    Ranked

--- data_base_management.py
+++ data_base_management.py
@@ -193,11 +193,86 @@ def make_tier_list(client, role):
     testDict = allDict[role]
     return testDict
     
-def get_ids(client):
+def get_ranks(client):
     mydb = client["Matches"]
     mycol = mydb["matches"]
+    playersId = []
+    ranks = {}
     for doc in mycol.find():
-        print(doc.get("_id"))
+        matches = list(doc.keys())
+        for match in matches:
+            if match != "_id": 
+                for player in doc[match].keys():
+                    if "player" in player:
+                        if doc[match][player]["PlayerId"] not in playersId:
+                            if doc[match][player]["Conquest_Tier"] in ranks.keys():
+                                ranks[doc[match][player]["Conquest_Tier"]] += 1
+                            else:
+                                ranks[doc[match][player]["Conquest_Tier"]] = 1
+
+                            if doc[match][player]["PlayerId"] != 0:
+                                playersId.append(doc[match][player]["PlayerId"])
+    return ranks
+
+
+def get_mmrs(client):
+    mydb = client["Matches"]
+    mycol = mydb["matches"]
+    playersId = []
+    mmrs = {
+        "0-250": 0,
+        "251-500": 0,
+        "501-750": 0,
+        "751-1000": 0,
+        "1001-1250": 0,
+        "1251-1500": 0,
+        "1501-1750": 0,
+        "1751-2000": 0,
+        "2001-2250": 0,
+        "2251-2500": 0,
+        "2501-2750": 0,
+        "2751-3000": 0,
+        "3001-3250": 0,
+        "3251-3500": 0,
+    }
+
+    for doc in mycol.find():
+        matches = list(doc.keys())
+        for match in matches:
+            if match != "_id": 
+                for player in doc[match].keys():
+                    if "player" in player:
+                        if doc[match][player]["PlayerId"] not in playersId:
+                            pass
+                            # if doc[match][player]["Ranked_Stat_Conq"] <= 250:
+                            #     mmrs["0-250"] +=1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 251 and doc[match][player]["Ranked_Stat_Conq"] <= 500:
+                            #     mmrs["251-500"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 501 and doc[match][player]["Ranked_Stat_Conq"] <= 750:
+                            #     mmrs["501-750"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 751 and doc[match][player]["Ranked_Stat_Conq"] <= 1000:
+                            #     mmrs["751-1000"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 1001 and doc[match][player]["Ranked_Stat_Conq"] <= 1250:
+                            #     mmrs["1001-1250"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 1251 and doc[match][player]["Ranked_Stat_Conq"] <= 1500:
+                            #     mmrs["1251-1500"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 1501 and doc[match][player]["Ranked_Stat_Conq"] <= 1750:
+                            #     mmrs["1501-1750"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 1751 and doc[match][player]["Ranked_Stat_Conq"] <= 2000:
+                            #     mmrs["1751-2000"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 2001 and doc[match][player]["Ranked_Stat_Conq"] <= 2250:
+                            #     mmrs["2001-2250"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 2251 and doc[match][player]["Ranked_Stat_Conq"] <= 2500:
+                            #     mmrs["2251-2500"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 2501 and doc[match][player]["Ranked_Stat_Conq"] <= 2750:
+                            #     mmrs["2501-2750"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 2751 and doc[match][player]["Ranked_Stat_Conq"] <= 3000:
+                            #     mmrs["2751-3000"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 3001 and doc[match][player]["Ranked_Stat_Conq"] <= 3250:
+                            #     mmrs["3000-3250"] += 1
+                            # elif doc[match][player]["Ranked_Stat_Conq"] > 3251 and doc[match][player]["Ranked_Stat_Conq"] <= 3500:
+                            #     mmrs["3251-3500"] += 1
+
 
 # roles = ["Carry", "Support", "Mid", "Jungle", "Solo"]
 # for role in roles: 
@@ -206,7 +281,6 @@ def get_ids(client):
 #     mycol = mydb["8/14/2021 - {}".format(role)]
 #     mycol.insert_one(tList)
 
-get_last_day(client)
 
 # if __name__ == "__main__":        
 #     for key in godsDict:
