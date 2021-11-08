54e565f4d8a203ea89146758a5396ac94b68a8be
MattK4517 <rotmgmayhem@gmail.com>
2021-11-08 11:16:46 -0500

    updated gitignore

--- hermesfrontend/src/components/drawer.js
+++ hermesfrontend/src/components/drawer.js
@@ -8,12 +8,6 @@ import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-
 import { Link } from "react-router-dom";
 import { HashRouter as HashRouter, Switch, Route} from "react-router-dom";
 import { Godpage, Gods, TierListPage, Match, Home, ContactForm, Items } from "./"
