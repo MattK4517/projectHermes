4fb5d71d755c50358ac9e0c4198d705b3cd6a9fd
MattK4517 <rotmgmayhem@gmail.com>
2021-11-08 13:04:37 -0500

    fixed hermes bot

--- hermesBot.py
+++ hermesBot.py
@@ -4,9 +4,10 @@
 import discord
 import sys
 import analyze as anlz
-import pandas as pd
+# import pandas as pd
 from main import client as dbClient
-from constants import Assassins, Guardians, Hunters, Mages, Warriors
+from constants import Assassins, Guardians, Hunters, Mages, Warriors, patch
+
 
 if __name__ == "__main__":
     token = open("token.txt", "r").read()  # I've opted to just save my token to a text file. 
@@ -37,25 +38,9 @@ if __name__ == "__main__":
                 for x in range(len(god)):
                     actgod += god[x]+" "
                 actgod = actgod.strip()
-                dataSheet = pd.read_excel("God Abilities & Items.xlsx", sheet_name="all_items")
-                data = anlz.get_top_builds_discord(mongo_client, actgod, role, req="discord")
-                build = data[0]
-                games = data[1][0]
-                wins = data[1][1]
-                wr = data[1][2]
+                data = anlz.get_top_builds(dbClient, actgod, role, patch)
                 ItemWR = []
-                print(build)
                 iconURL = anlz.get_url(actgod)
-                for slot in build:
-                    if build[slot]["item1"]["games"] != 0:
-                        ItemWR.append(round(build[slot]["item1"]["wins"]/build[slot]["item1"]["games"] * 100, 2))
-                    else:
-                        ItemWR.append(0)
-                    if build[slot]["item2"]["games"] != 0:
-                        ItemWR.append(round(build[slot]["item2"]["wins"]/build[slot]["item2"]["games"] * 100, 2))
-                    else:
-                        ItemWR.append(0)
-                    
                 if actgod in Assassins:
                      color = "fce703"
                 elif actgod in Guardians:
@@ -66,14 +51,16 @@ if __name__ == "__main__":
                     color = "9a1af0"
                 elif actgod in Warriors:
                     color = "fc0303"
-                embed=discord.Embed(title=actgod+" "+role+" Build", description="Games: {} | Wins: {} | WR: {}".format(games, wins, wr), color = int(color, base=16))
-                embed.set_thumbnail(url=iconURL["url"])
-                embed.add_field(name="Slot 1", value=build["slot1"]["item1"]["item"]+" WR: "+str( ItemWR[0] )+"%\n"+build["slot1"]["item2"]["item"]+" WR: "+str( ItemWR[1] )+"%", inline=True)
-                embed.add_field(name="Slot 2", value=build["slot2"]["item1"]["item"]+" WR: "+str( ItemWR[2] )+"%\n"+build["slot2"]["item2"]["item"]+" WR: "+str( ItemWR[3] )+"%", inline=True)
-                embed.add_field(name="Slot 3", value=build["slot3"]["item1"]["item"]+" WR: "+str( ItemWR[4] )+"%\n"+build["slot3"]["item2"]["item"]+" WR: "+str( ItemWR[5] )+"%", inline=True)
-                embed.add_field(name="Slot 4", value=build["slot4"]["item1"]["item"]+" WR: "+str( ItemWR[6] )+"%\n"+build["slot4"]["item2"]["item"]+" WR: "+str( ItemWR[7] )+"%", inline=True)
-                embed.add_field(name="Slot 5", value=build["slot5"]["item1"]["item"]+" WR: "+str( ItemWR[8] )+"%\n"+build["slot5"]["item2"]["item"]+" WR: "+str( ItemWR[9] )+"%", inline=True)
-                embed.add_field(name="Slot 6", value=build["slot6"]["item1"]["item"]+" WR: "+str( ItemWR[10] )+"%\n"+build["slot6"]["item2"]["item"]+" WR: "+str( ItemWR[11] )+"%", inline=True)
+                embed=discord.Embed(title=f"{actgod} {role} Build", description="Games: {} | Wins: {} | WR: {} \n [See more info here](https://www.smitestats.gg/#/{})".format(data["games"], data["wins"], data["winRate"], actgod.replace(" ", "_")), color = int(color, base=16))
+                embed.set_thumbnail(url=iconURL)
+                for i, slot in enumerate(data):
+                    if "slot" in slot:
+                        item1 = data[slot]["item1"]["item"]
+                        item2 = data[slot]["item2"]["item"]
+                        item1WR = round(data[slot]["item1"]["wins"]/data[slot]["item1"]["games"]*100 , 2)
+                        item2WR = round(data[slot]["item2"]["wins"]/data[slot]["item2"]["games"]*100, 2)
+                        embed.add_field(name=f"Slot {i+1}", value=f"{item1} WR: {item1WR}%\n {item2} WR: {item2WR}%", inline=True)
+                    
                 await message.channel.send(embed=embed)
         
         if message.content.startswith("$matchups"):
@@ -87,15 +74,8 @@ if __name__ == "__main__":
                 for x in range(len(god)):
                     actgod += god[x]+" "
                 actgod = actgod.strip()
-                print(actgod)
-                data = anlz.get_worst_matchups(mongo_client, actgod, role, req="discord")
-                matchups = data[0]
-                games = data[1][0]
-                wins = data[1][1]
-                wr = data[1][2]
+                data = anlz.get_worst_matchups(mongo_client, actgod, role, patch)
                 iconURL = anlz.get_url(actgod)
-                
-
                 if actgod in Assassins:
                     color = "fce703"
                 elif actgod in Guardians:
@@ -106,10 +86,43 @@ if __name__ == "__main__":
                     color = "9a1af0"
                 elif actgod in Warriors:
                     color = "fc0303"
-                embed=discord.Embed(title=actgod+" "+role+" Matchups", description="Games: {} | Wins: {} | WR: {}".format(games, wins, wr), color = int(color, base=16))
-                embed.set_thumbnail(url=iconURL["url"])
-                for matchup in matchups:
-                    embed.add_field(name=matchup, value="Games Played: "+str(matchups[matchup]["timesPlayed"])+" | WR: "+str(matchups[matchup]["winRate"])+"%", inline=True)
+                embed=discord.Embed(title=actgod+" "+role+" Worst Matchups", description="Games: {} | Wins: {} | WR: {} \n [See more info here](https://www.smitestats.gg/#/{})".format(data["games"], data["wins"], data["winRate"], actgod.replace(" ", "_")), color = int(color, base=16))
+                embed.set_thumbnail(url=iconURL)
+                for i, matchup in enumerate(data):
+                    if matchup not in ["games", "wins", "winRate"] and i < 4:
+                        embed.add_field(name=matchup, value="Games Played: "+str(data[matchup]["timesPlayed"])+"\nWR: "+str(data[matchup]["winRate"])+"%", inline=True)
                 await message.channel.send(embed=embed)
+        
+        if message.content.startswith("$paths"):
+            m = message.content.split(" ")
+            if len(m) < 3:
+                await message.channel.send("Must use $god role format")
+            else:
+                actgod = ""
+                god = m[1:len(m)-1]
+                role = m[-1]
+                for x in range(len(god)):
+                    actgod += god[x]+" "
+                actgod = actgod.strip()
+                data = anlz.get_build_path(dbClient, actgod, role, patch)
+                iconURL = anlz.get_url(actgod)
+                if actgod in Assassins:
+                    color = "fce703"
+                elif actgod in Guardians:
+                    color = "067527"
+                elif actgod in Hunters:
+                    color = "754306"
+                elif actgod in Mages:
+                    color = "9a1af0"
+                elif actgod in Warriors:
+                    color = "fc0303"
+                embed=discord.Embed(title=actgod+" "+role+" Worst Matchups", description="[See more info here](https://www.smitestats.gg/#/{})".format(actgod.replace(" ", "_")), color = int(color, base=16))
+                embed.set_thumbnail(url=iconURL)
+                for i, path in enumerate(data):
+                    if i < 2:
+                        games = data[path]["wins"]+data[path]["losses"]
+                        embed.add_field(name=f"Path {i+1}", value="{}\nGames: {} Win Rate: {}".format(path.replace(",", ", "), games, round(data[path]["wins"]/games * 100,2)), inline=True)
+                await message.channel.send(embed=embed)
+                
 
     client.run(token)
--- main.py
+++ main.py
@@ -14,57 +14,58 @@ client = pymongo.MongoClient(
     "mongodb+srv://sysAdmin:vJGCNFK6QryplwYs@cluster0.7s0ic.mongodb.net/Cluster0?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs="CERT_NONE")
 
 if __name__ == "__main__":
-    # starttime = datetime.now()
-    # with open("cred.txt", "r") as creds:
-    #     lines = creds.readlines()
-    #     smite_api = SmiteAPI(devId=lines[0].strip(), authKey=lines[1].strip(), responseFormat=pyrez.Format.JSON)
-    # get_new_id(client, smite_api)
-    # run_pull(patch, "20211020")
-    # run_format(patch, "10/19/2021")
-    mydb = client["single_items"]
-    mycol = mydb["Kali"]
-    dmg_dict = {}
-    index = 0
-    builds = {}
-    index = 0
-    for x in mycol.aggregate([
-            { 
-                "$match": {"role_played": "Jungle", "patch": "8.10", "rank": "Gold" } 
-            }, 
-            { 
-                "$group": {"_id": {"slot1": "$Kali.slot1", "slot2": "$Kali.slot2", "slot3": "$Kali.slot3", "win_status": "$win_status"}, "count": {"$sum": 1} }
-            },
-            {
-                "$sort" : {"count": 1}
-            }
-    ]):
-        if "{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"]) not in builds.keys():
-            builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])] = { 
-                "slot1": x["_id"]["slot1"],
-                "slot2": x["_id"]["slot2"],
-                "slot3": x["_id"]["slot3"],
-                "wins": 0,
-                "losses": 0,
-                }
-        if x["_id"]["win_status"] == "Winner":
-            builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])]["wins"] += x["count"]
-        elif x["_id"]["win_status"] == "Loser":
-            builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])]["losses"] += x["count"]
-        index += 1
+    print(anlz.get_build_path(client, "Achilles", "Solo", patch))
+    # # starttime = datetime.now()
+    # # with open("cred.txt", "r") as creds:
+    # #     lines = creds.readlines()
+    # #     smite_api = SmiteAPI(devId=lines[0].strip(), authKey=lines[1].strip(), responseFormat=pyrez.Format.JSON)
+    # # get_new_id(client, smite_api)
+    # # run_pull(patch, "20211020")
+    # # run_format(patch, "10/19/2021")
+    # mydb = client["single_items"]
+    # mycol = mydb["Kali"]
+    # dmg_dict = {}
+    # index = 0
+    # builds = {}
+    # index = 0
+    # for x in mycol.aggregate([
+    #         { 
+    #             "$match": {"role_played": "Jungle", "patch": "8.10", "rank": "Gold" } 
+    #         }, 
+    #         { 
+    #             "$group": {"_id": {"slot1": "$Kali.slot1", "slot2": "$Kali.slot2", "slot3": "$Kali.slot3", "win_status": "$win_status"}, "count": {"$sum": 1} }
+    #         },
+    #         {
+    #             "$sort" : {"count": 1}
+    #         }
+    # ]):
+    #     if "{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"]) not in builds.keys():
+    #         builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])] = { 
+    #             "slot1": x["_id"]["slot1"],
+    #             "slot2": x["_id"]["slot2"],
+    #             "slot3": x["_id"]["slot3"],
+    #             "wins": 0,
+    #             "losses": 0,
+    #             }
+    #     if x["_id"]["win_status"] == "Winner":
+    #         builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])]["wins"] += x["count"]
+    #     elif x["_id"]["win_status"] == "Loser":
+    #         builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])]["losses"] += x["count"]
+    #     index += 1
 
-    to_remove = []
-    for build in builds:
-        if (builds[build]["wins"] + builds[build]["losses"]) < index * 1.5/100:
-            to_remove.append(build)
+    # to_remove = []
+    # for build in builds:
+    #     if (builds[build]["wins"] + builds[build]["losses"]) < index * 1.5/100:
+    #         to_remove.append(build)
     
-    for element in to_remove:
-        del builds[element]
+    # for element in to_remove:
+    #     del builds[element]
 
-    test_sort = OrderedDict(sorted(builds.items(),
-            key = lambda x: getitem(x[1], "wins")))
+    # test_sort = OrderedDict(sorted(builds.items(),
+    #         key = lambda x: getitem(x[1], "wins")))
     
-    builds = dict(test_sort)
-    print(builds)
+    # builds = dict(test_sort)
+    # print(builds)
                 
 
 ## 5 looops 2507 matches in 2:23:46
