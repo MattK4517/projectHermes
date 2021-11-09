0aa061057f89ade8b4daa3f5c5610fb78b37799a
MattK4517 <rotmgmayhem@gmail.com>
2021-11-08 22:02:54 -0500

    Responsive Appbar

--- hermesfrontend/src/App.js
+++ hermesfrontend/src/App.js
@@ -3,159 +3,24 @@ import React, {useState} from "react";
 import { HashRouter as HashRouter, Switch, Route} from "react-router-dom";
 import { Godpage, Gods, TierListPage, Match, Home, ContactForm, Items, MiniDrawer } from "./components";
 import { TierList } from "./components/Tierlists/TierList";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import { green } from '@mui/material/colors';
 
-// const godsDict = {
-//   "Achilles": "Solo",
-//   "Agni": "Mid",
-//   "Ah Muzen Cab": "Carry",
-//   "Ah Puch": "Mid",
-//   "Amaterasu": "Solo",
-//   "Anhur": "Carry",
-//   "Anubis": "Mid",
-//   "Ao Kuang": "Jungle",
-//   "Aphrodite": "Mid",
-//   "Apollo": "Carry",
-//   "Arachne": "Jungle",
-//   "Ares": "Support",
-//   "Artemis": "Carry",
-//   "Artio": "Support",
-//   "Athena": "Support",
-//   "Awilix": "Jungle",
-//   "Baba Yaga": "Mid",
-//   "Bacchus": "Supprt",
-//   "Bakasura": "Jungle",
-//   "Baron Samedi": "Mid",
-//   "Bastet": "Jungle",
-//   "Bellona": "Solo",
-//   "Cabrakan": "Support",
-//   "Camazotz": "Jungle",
-//   "Cerberus": "Support",
-//   "Cernunnos": "Carry",
-//   "Chaac": "Solo",
-//   "Chang\'e": "Mid",
-//   "Charybdis": "Carry",
-//   "Chernobog": "Carry",
-//   "Chiron": "Carry",
-//   "Chronos": "Carry",
-//   "Cthulhu": "Support",
-//   "Cu Chulainn": "Solo",
-//   "Cupid": "Carry",
-//   "Da Ji": "Jungle",
-//   "Danzaburou": "Carry",
-//   "Discordia": "Mid",
-//   "Erlang Shen": "Jungle",
-//   "Eset": "Mid",
-//   "Fafnir": "Support",
-//   "Fenrir": "Jungle",
-//   "Freya": "Carry",
-//   "Ganesha": "Support",
-//   "Geb": "Support",
-//   "Gilgamesh": "Solo",
-//   "Guan Yu": "Solo",
-//   "Hachiman": "Carry",
-//   "Hades": "Mid",
-//   "He Bo": "Mid",
-//   "Heimdallr": "Carry",
-//   "Hel": "Mid",
-//   "Hera": "Mid",
-//   "Hercules": "Solo",
-//   "Horus": "Support",
-//   "Hou Yi": "Carry",
-//   "Hun Batz": "Jungle",
-//   "Izanami": "Carry",
-//   "Janus": "Mid",
-//   "Jing Wei": "Carry",
-//   "Jormungandr": "Solo",
-//   "Kali": "Jungle",
-//   "Khepri": "Support",
-//   "King Arthur": "Solo",
-//   "Kukulkan": "Mid",
-//   "Kumbhakarna": "Support",
-//   "Kuzenbo": "Support",
-//   "Loki": "Jungle",
-//   "Medusa": "Carry",
-//   "Mercury": "Jungle",
-//   "Merlin": "Mid",
-//   "Morgan Le Fay": "Mid",
-//   "Mulan": "Solo",
-//   "Ne Zha": "Jungle",
-//   "Neith": "Carry",
-//   "Nemesis": "Jungle",
-//   "Nike": "Solo",
-//   "Nox": "Mid",
-//   "Nu Wa": "Mid",
-//   "Odin": "Solo",
-//   "Olorun": "Carry",
-//   "Osiris": "Solo",
-//   "Pele": "Jungle",
-//   "Persephone": "Mid",
-//   "Poseidon": "Mid",
-//   "Ra": "Mid",
-//   "Raijin": "Mid",
-//   "Rama": "Carry",
-//   "Ratatoskr": "Jungle",
-//   "Ravana": "Jungle",
-//   "Scylla": "Mid",
-//   "Serqet": "Jungle",
-//   "Set": "Jungle",
-//   "Skadi": "Carry",
-//   "Sobek": "Support",
-//   "Sol": "Carry",
-//   "Sun Wukong": "Solo",
-//   "Susano": "Jungle",
-//   "Sylvanus": "Support",
-//   "Terra": "Support",
-//   "Thanatos": "Jungle",
-//   "The Morrigan": "Mid",
-//   "Thor": "Jungle",
-//   "Thoth": "Mid",
-//   "Tiamat": "Mid",
-//   "Tsukuyomi": "Jungle",
-//   "Tyr": "Solo",
-//   "Ullr": "Carry",
-//   "Vamana": "Solo",
-//   "Vulcan": "Mid",
-//   "Xbalanque": "Carry",
-//   "Xing Tian": "Support",
-//   "Yemoja": "Support",
-//   "Ymir": "Support",
-//   "Zeus": "Mid",
-//   "Zhong Kui": "Solo"
-// }
-// let routes = [
-//   {
-//     path: "/",
-//     component: <Home />,
-//   },
-//   {
-//     path: "/Gods",
-//     component: <Gods />,
-//   },
-//   {
-//     path: "/tierlist",
-//     component: <TierListPage />,
-//   },
-//   {
-//     path: "/contact",
-//     component: <ContactForm />
-//   },
-//   {
-//     path: "/match",
-//     component: <Match />
-//   }
-// ]
-// Object.keys(godsDict).forEach((god) => {
-//   routes = [...routes, {
-//     path: "/".concat(god).replaceAll(" ", "_"),
-//     component: <Godpage god={god} role={godsDict[god]}/>
-//   },
-//   {
-//     path: "/".concat(god.replaceAll(" ", "_"), "/items"),
-//     component: <Items god={god} role={godsDict[god]}/>
-//   }
-// ]
-// })
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
 
 class App extends React.Component {
   render () {
@@ -164,7 +29,9 @@ class App extends React.Component {
     <div className="App">
       <HashRouter>
         <div className="page-content">
-        <MiniDrawer />
+        <ThemeProvider theme={theme}>
+          <MiniDrawer />
+        </ThemeProvider>
         </div>
       </HashRouter>
     </div>
