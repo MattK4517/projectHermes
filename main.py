f9a767c62a68db21a52d800cb7ae4a4b1865f17e
Matt <rotmgmayhem@gmail.com>
2021-11-13 01:19:03 -0500

    prelim item data for single matchups

--- main.py
+++ main.py
@@ -14,63 +14,60 @@ client = pymongo.MongoClient(
     "mongodb+srv://sysAdmin:9gR7C1aDKclng4jA@cluster0.7s0ic.mongodb.net/Cluster0?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs="CERT_NONE")
 
 if __name__ == "__main__":
-    god = "Kali"
-    role = "Jungle"
-    rank = "All Ranks"
-    patch = "8.10"
-    mydb = client["single_items"]
+    god = "Achilles"
+    mydb = client["single_matchups"]
     mycol = mydb[god]
-    index = 0
-    builds = {}
-    if "All" not in rank:
-        myquery = {"role_played": role, "patch": patch, "rank": rank}
-    else:
-        myquery = {"role_played": role, "patch": patch}
-    
-    for x in mycol.aggregate(
-        [
-            {
-                "$match": myquery,
-            },
-            {
-                "$group": {
-                    "_id": {
-                        "slot1": f"${god}.slot1",
-                        "slot2": f"${god}.slot2",
-                        "slot3": f"${god}.slot3",
-                        "win_status": "$win_status",
-                    },
-                    "count": {"$sum": 1},
-                }
-            },
-            {"$sort": {"count": 1}},
+    match_ids = []
+    for x in mycol.find({"enemy": "Anubis", "patch": "8.10", "role_played": "Solo"}):
+        match_ids.append(x["matchId"])
+
+    builds = []
+    itemsdb = client["single_items"]
+    itemscol = itemsdb[god]
+    games = 0
+    for x in itemscol.aggregate([
+        {
+                "$match": {"matchId": {"$in": match_ids}},
+        },
+            # {
+            #     "$group": {
+            #         "_id": {
+            #             "slot1": f"${god}.slot1",
+            #             "slot2": f"${god}.slot2",
+            #             "slot3": f"${god}.slot3",
+            #             "win_status": "$win_status",
+            #         },
+            #         "count": {"$sum": 1},
+            #     }
+            # },
+            # {"$sort": {"count": 1}},
         ]
     ):
-        if "{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"]) not in builds.keys():
-            builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])] = { 
-                "slot1": x["_id"]["slot1"],
-                "slot2": x["_id"]["slot2"],
-                "slot3": x["_id"]["slot3"],
-                "wins": 0,
-                "losses": 0,
-                }
-        if x["_id"]["win_status"] == "Winner":
-            builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])]["wins"] += x["count"]
-        elif x["_id"]["win_status"] == "Loser":
-            builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])]["losses"] += x["count"]
-        index += 1
+        builds.append(x)
+        # games += x["count"]
+        # if "{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"]) not in builds.keys():
+        #     builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])] = { 
+        #         "slot1": x["_id"]["slot1"],
+        #         "slot2": x["_id"]["slot2"],
+        #         "slot3": x["_id"]["slot3"],
+        #         "wins": 0,
+        #         "losses": 0,
+        #         }
+        # if x["_id"]["win_status"] == "Winner":
+        #     builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])]["wins"] += x["count"]
+        # elif x["_id"]["win_status"] == "Loser":
+        #     builds["{},{},{}".format(x["_id"]["slot1"], x["_id"]["slot2"], x["_id"]["slot3"])]["losses"] += x["count"]
 
-    to_remove = []
-    for build in builds:
-        if (builds[build]["wins"] + builds[build]["losses"]) < index * 1.5/100:
-            to_remove.append(build)
-    
-    for element in to_remove:
-        del builds[element]
 
-    test_sort = OrderedDict(sorted(builds.items(),
-            key = lambda x: getitem(x[1], "wins")))
-    print(builds)
+    # print(builds)
+    # print(games)
+    # builds = []
+    # for matchId in match_ids:
+    #     for x in itemscol.find({"matchId": matchId}, {"_id": 0, "Achilles": 1, "win_status": 1}):
+    #         builds.append({**x["Achilles"], **{"win_status": x["win_status"]}})
+
+    # print(builds)
+    print(anlz.get_top_builds(client, "Achilles", "Solo", "8.10", data=builds))
                 
 
 ## 5 looops 2507 matches in 2:23:46
