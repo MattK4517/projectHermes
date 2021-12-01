236a69c02813ed4cbffa6a94c1d9730d211d7aa4
MattK4517 <rotmgmayhem@gmail.com>
2021-12-01 13:09:22 -0500

    rss feed homepage

--- hermesfrontend/src/components/Home.jsx
+++ hermesfrontend/src/components/Home.jsx
@@ -1,25 +1,26 @@
 import { Box, Container } from "@material-ui/core";
 import React from "react";
 import { Link } from "react-router-dom";
-
+import RSSFeeder from "./RssFeed";
 function Home() {
   return (
-    <Container maxWidth="100%">
-      <Box>
-        <div className="home">
-          <div className="home-image">
-            <div className="new-god content-section">
-              <h3>
-                NEW GOD CHARYBDIS
-              </h3>
-              <h4>
-                <Link to={"/".concat(("Charybdis").replaceAll(" ","_"))}>See how they stack up!</Link> 
-              </h4>
-            </div>
-          </div>
-        </div>
-      </Box>
-    </Container>
+    <RSSFeeder />
+    // <Container maxWidth="100%">
+    //   <Box>
+    //     <div className="home">
+    //       <div className="home-image">
+    //         <div className="new-god content-section">
+    //           <h3>
+    //             NEW GOD CHARYBDIS
+    //           </h3>
+    //           <h4>
+    //             <Link to={"/".concat(("Charybdis").replaceAll(" ","_"))}>See how they stack up!</Link> 
+    //           </h4>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </Box>
+    // </Container>
   );
 }
 
