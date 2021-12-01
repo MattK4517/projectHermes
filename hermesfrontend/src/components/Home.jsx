04326de0c976088b152a5dd3af13c00fcb71397c
Matt <rotmgmayhem@gmail.com>
2021-12-01 18:14:39 -0500

    commit

--- hermesfrontend/src/components/Home.jsx
+++ hermesfrontend/src/components/Home.jsx
@@ -1,24 +1,14 @@
 import { Box, Container } from "@material-ui/core";
 import React from "react";
 import { Link } from "react-router-dom";
+import BuildPage from "./mainGodPage/testPage"
 
 function Home() {
   return (
     <Container maxWidth="100%">
-      <Box>
-        <div className="home">
-          <div className="home-image">
-            <div className="new-god content-section">
-              <h3>
-                NEW GOD CHARYBDIS
-              </h3>
-              <h4>
-                <Link to={"/".concat(("Charybdis").replaceAll(" ","_"))}>See how they stack up!</Link> 
-              </h4>
-            </div>
-          </div>
-        </div>
-      </Box>
+        <BuildPage>
+
+        </BuildPage>
     </Container>
   );
 }
