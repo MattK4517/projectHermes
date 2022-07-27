d029338f8b380701a629076abc121e98b4f1d21b
MattK <75119334+MattK4517@users.noreply.github.com>
2022-07-27 13:20:42 -0400

    Smitestats 067 damage calc documentation (#109)
    * add comments to calc_combo_damage_raw
    
    * add comments to funcitons

--- damage_calculator.py
+++ damage_calculator.py
@@ -7,6 +7,9 @@ from damage_calc_helpers import *
 
 
 def get_num_hits(god, ability, base):
+    """
+        returns the number of times each ability hits
+    """
     if special_case(ability, base):
         return special_case(ability, base)
 
@@ -22,6 +25,9 @@ def get_num_hits(god, ability, base):
 
 
 def get_scaling_changes(god, ability, hit):
+    """
+        returns if ability has any changes to its damage scaling, and what said scaling is 
+    """
     ability_key = ""
     for key in num_hits_dict[god]:
         if key in ability:
@@ -32,6 +38,10 @@ def get_scaling_changes(god, ability, hit):
 
 
 def get_percent_change(god, ability, hit):
+    """
+      returns if ability has any changes to its damage scaling, and what said scaling is
+      #TODO remove funciton
+    """
     ability_key = ""
     for key in num_hits_dict[god]:
         if key in ability:
@@ -42,6 +52,9 @@ def get_percent_change(god, ability, hit):
 
 
 def calc_ability_damage_raw(base, scaling, power, god, ability, prot, miti, armor_reduction_per, armor_reduction_flat, pen_per, pen_flat, **procs):
+    """
+      returns damage stats for a given ability
+    """
     proc_damage = 0
     damage = 0
     for i in range(get_num_hits(god, ability, base)):
@@ -65,7 +78,18 @@ def calc_ability_damage_raw(base, scaling, power, god, ability, prot, miti, armo
     }
 
 
-def calc_combo_damage_raw(client, god, levels, power, build, enemy, enemy_build, level=20, enemy_level=20):
+def calc_combo_damage_raw(client, god, levels, build, enemy, enemy_build, level=20, enemy_level=20):
+    """ returns json for a gods damage stats given parameters
+    Args:
+        client ([MongoClient]): database connection
+        god ([String]): name of god
+        levels ([Dict]): ability level for base damage, example in main function
+        build ([String[]]): list of items to get stats for god
+        enemy ([String]): enemy god to get base prots for
+        enemy_build ([String[]]): list of items to get stats for enemy god
+        level ([Int]): level of god (for base stats)
+        enemy_level ([Int]): level of enemy (for base stats)
+    """
     temp = anlz.get_god_stats(client, god, level)
     attSpeed, power, critChance, armor_reduction_per, armor_reduction_flat, pen_per, pen_flat = calc_dps_stats(
         client, god, build, temp["AttackSpeed"])
@@ -163,6 +187,9 @@ def get_special_item(item):
 
 
 def calc_dps_stats(client, god, build, baseAttSpeed):
+    """
+      returns gods offensive stats with a given build
+    """
     myfilter = {
         **{"_id": 0},
         **{f"AttackSpeed": 1}
@@ -209,6 +236,9 @@ def calc_dps_stats(client, god, build, baseAttSpeed):
 
 
 def calc_tank_stats(client, god, build):
+    """
+      returns gods defensive stats with a given build
+    """
     god = god.title()
     print(god)
     physProt = 0
@@ -232,6 +262,9 @@ def calc_tank_stats(client, god, build):
 
 
 def calc_qins_dmg(hp):
+    """
+        returns damage of qins sais given hp scaling
+    """
     dmg = 0
     if hp <= 2000:
         dmg = round(hp * .03)
@@ -244,6 +277,10 @@ def calc_qins_dmg(hp):
 
 
 def calc_auto_dmg(god, power):
+    """
+      returns basic attack damage of gods
+      #TODO add magical characters
+    """
     if (god.lower() in [assassin.lower() for assassin in Assassins]
         or god.lower() in [warrior.lower() for warrior in Warriors]
             or god.lower() in [hunter.lower() for hunter in Hunters]):
@@ -253,6 +290,10 @@ def calc_auto_dmg(god, power):
 
 
 def calc_dps(client, god, build, enemy, enemy_build, enemy_level, level=20):
+    """
+      returns damage stats for basic attacks
+      #TODO clean up entire function
+    """
     temp = anlz.get_god_stats(client, god, level)
     attSpeed, power, critChance, armor_reduction_per, armor_reduction_flat, pen_per, pen_flat = calc_dps_stats(
         client, god, build, baseAttSpeed=temp["AttackSpeed"])
@@ -375,6 +416,9 @@ def calc_dps(client, god, build, enemy, enemy_build, enemy_level, level=20):
 
 
 def calc_mitigation(dmg, prot, miti, armor_reduction_per, armor_reduction_flat, pen_per, pen_flat):
+    """
+      returns how much damage was mitigated given pen/prot stats
+    """
     # print(armor_reduction_per, armor_reduction_flat, pen_per, pen_flat)
     # % armor reduction
     # Flat armor reduction
@@ -391,6 +435,9 @@ def calc_mitigation(dmg, prot, miti, armor_reduction_per, armor_reduction_flat, 
 
 
 if __name__ == "__main__":
+    # ADJUST ABILITY LEVELS HERE
+    # TO GET A RANK 1 ACHILLES 1 FOR EXAMPLE
+    # set "1" to 1
     levels = {
         "1": 4,
         "2": 5,
@@ -398,27 +445,6 @@ if __name__ == "__main__":
         "4": 1,
         "5": 5
     }
-    # print(calc_dps(client, "Achilles", [
-    #     "Odysseus' Bow",
-    #     "Toxic Blade",
-    #     "Shogun's Kusari",
-    #     "Silverbranch Bow",
-    #     "Ichaival"
-    # ], "Odin", [
-    #     "Bluestone Brooch",
-    #     "Runeforged Hammer",
-    #     "Breastplate of Determination",
-    #     "Bulwark of Hope",
-    #     "Pridwen",
-    #     "Spectral Armor"
-    # ], 20, 20))
-    # avg = 0
-    # for i in range(10):
-    # calc_dps(client, "Achilles", ["Manikin Mace", "Bloodforge", "Evolved Rage", "Serrated Edge", "Deathbringer", "Wind Demon"],
-    # "Agni", [], 20, 20)
-    # print(avg/10)
-    # for guardian in Guardians:
+    
     print(calc_combo_damage_raw(client, "Chaac",
-          levels, 0, ["Bluestone Pendant"], "Baron Samedi", [], 5, 6))
-
-    # 470 + 83
+          levels, ["Bluestone Pendant"], "Baron Samedi", [], 5, 6))
